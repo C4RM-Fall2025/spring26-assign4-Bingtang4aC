d78d2f85bb06b99d387fdaa03b6992a96a0d37f8
Bingtang4aC <yj2937@columbia.edu>
2026-02-22 20:13:51 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,28 @@
+def PriceBond(y, face, couponRate, m, ppy=1):
+    pvcfsum = 0
+    cf = face * couponRate / ppy  
+    
+    for i in range(1, (m * ppy)+1):
+        pvm = (1 + y / ppy) ** (-i)
+        pvcf = pvm * cf
+        pvcfsum = pvcfsum + pvcf
+
+        if i == m * ppy: 
+            pvcfsum = pvcfsum + face * pvm
+
+    return pvcfsum
+
+y = 0.03
+face = 2_000_000
+couponRate = 0.04
+m = 10
 
 
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+ppy = 1
+price_1 = PriceBond(y, face, couponRate, m, ppy)
+print(price_1)
+
+
+ppy = 2
+price_2 = PriceBond(y, face, couponRate, m, ppy)
+print(price_2)
